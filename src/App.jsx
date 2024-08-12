558e4d2bc0915a3e28caba735032d2ce9c4f0f94
RonanKer29 <kervella.ronan@hotmail.com>
2024-08-12 16:21:42 +0200

    UserInput component, with fields + setting up the useState for it

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import Header from "./components/Header.jsx";
 import UserInput from "./components/UserInput.jsx";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header /> <UserInput />
+    </>
+  );
 }
 
 export default App;
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,3 +1,35 @@
+import { useState } from "react";
+
 export default function UserInput() {
-  return;
+  const [UserInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>INITIAL INVESTMENT</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label>ANNUAL INVESTMENT</label>
+          <input type="number" required />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>EXPECTED RETURN</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label>DURATION</label>
+          <input type="number" required />
+        </p>
+      </div>
+    </section>
+  );
 }
